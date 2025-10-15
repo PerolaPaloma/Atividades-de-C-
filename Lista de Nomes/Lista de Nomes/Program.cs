af635479f48d67756843fc8a3af22e256e38be37
Pérola Paloma <perolapaloma1@gmail.com>
2025-10-15 17:07:55 -0300

    Add files via upload

---
+++ Lista de Nomes/Lista de Nomes/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> nomes = new List<string>();
+        string entrada;
+        int resposta = 0;
+        do
+        {
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("Insira os nomes para ser cadastrados");
+            Console.WriteLine("------------------------------------");
+
+            Console.Write("Nome: ");
+            entrada = Console.ReadLine().ToUpper();
+            Console.Clear();    
+
+            Console.WriteLine("Deseja continuar");
+            Console.WriteLine(" 1 - Sim");
+            Console.WriteLine(" 2 - Não");
+            Console.WriteLine("------------------------------------");
+
+            resposta = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("------------------------------------");
+
+            Console.Clear();
+
+            if (!string.IsNullOrWhiteSpace(entrada))
+                nomes.Add(entrada);
+            
+        }
+
+        while (resposta == 1);
+        {
+            
+        }
+        nomes.Sort();
+
+        Console.WriteLine("\nNomes cadastrados:");
+        foreach (string nome in nomes)
+        {
+            Console.WriteLine("- " + nome);
+        }
+
+        Console.WriteLine($"\nTotal de nomes cadastrados: {nomes.Count}");
+    }
+}
---
+++ contador/contador/Program.cs
@@ -0,0 +1,105 @@
+﻿Console.WriteLine("Digite o numero inicial: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o numero final: ");
+int num2 = int.Parse(Console.ReadLine());
+int resposta = 0;
+
+while (resposta < 1 || resposta > 3)
+{
+    Console.WriteLine("Como você deseja realizar a contagem: ");
+    Console.WriteLine(" 1 - Normal. ");
+    Console.WriteLine(" 2 - Somente os numeros pares. ");
+    Console.WriteLine(" 3 - Somente os numeros impares. ");
+    Console.WriteLine("--------------------------------------");
+
+
+    resposta = int.Parse(Console.ReadLine());
+    Console.WriteLine("--------------------------------------");
+
+    switch (resposta)
+    {
+        case 1:
+
+            if (num1 <= num2)
+            {
+                for (int i = num1; i <= num2; i++)
+
+                    Console.WriteLine(i);
+            }
+            else
+            {
+                for (int i = num1; i >= num2; i--)
+
+                    Console.WriteLine(i);
+
+            }
+            Console.WriteLine("FIM");
+
+            break;
+
+        case 2:
+            if (num1 <= num2)
+            {
+                for (int i = num1; i <= num2; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+            else
+            {
+                for (int i = num1; i >= num2; i--)
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+
+                }
+            }
+
+            Console.WriteLine("FIM");
+
+            break;
+
+        case 3:
+            if (num1 <= num2)
+            {
+                for (int i = num1; i <= num2; i++)
+                {
+                    if (i % 2 == 1)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+            else
+            {
+                for (int i = num1; i >= num2; i--)
+                {
+                    if (i % 2 == 1)
+                    {
+                        Console.WriteLine(i);
+                    }
+
+                }
+            }
+
+            Console.WriteLine("FIM");
+
+            break;
+        default:
+            Console.WriteLine("ERRO. Selecione uma opcao valida.");
+            Console.WriteLine("--------------------------------------");
+
+
+            break;
+    }
+}
+
+
+
+
